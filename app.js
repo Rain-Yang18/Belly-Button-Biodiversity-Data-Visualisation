22bb525552a381c5aa31d8c1b54486626d4edbac
RAIN YANG <RainYang@Rains-MacBook.home>
2021-05-22 01:00:09 +1000

    Update

--- app.js
+++ app.js
@@ -148,31 +148,3 @@ function insertinfo (id) {
 
   })
 }
-
-// function buildGauge() {
-//   d3.json("samples.json").then(function(data) {
-    
-//     console.log(data)
-
-//     // Filter the data when the id is the same at metadata array
-//     var sample = data.metadata.filter(x => x.id == id)[0];
-//     // console.log(sample);
-  
-//     var washingfreq = sample.wfreq;
-
-//     var data = [
-//       {
-//         domain: { x: [0, 1], y: [0, 1] },
-//         value: 270,
-//         title: { text: "Speed" },
-//         type: "indicator",
-//         mode: "gauge+number"
-//       }
-//     ];
-    
-//     var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
-//     Plotly.newPlot('myDiv', data, layout);
-
-//   })
-
-// }
